38eb2bd0976c8a17a082ac17686b04a13ae27ae2
sivid <sivid13@gmail.com>
2014-06-13 11:31:43 +0800

    homework 3 almost done missing q2, static

--- III_Java_Class_OOP/src/homework_3/Stack.java
+++ III_Java_Class_OOP/src/homework_3/Stack.java
@@ -22,47 +22,45 @@ class StackFullException extends Exception{
 
 public class Stack {
 	int[] stack = new int[10];
-	public int top = -1;		// when top=-1, stack is empty, normal
+	int top = -1;		// when top=-1, stack is empty, normal
 	
 	public void push(int data) throws StackFullException {
-		if (top==stack.length-1){		// stack will explode if we don't do something
+		if (top >= stack.length-1){		// stack will explode if we don't do something
 			throw new StackFullException(top);
 		}
 		stack[++top] = data;
 		System.out.println("push, stack[" + top + "] = " + data);
 	}
-	public int pop(){
+	public int pop() throws StackEmptyException {
+		if (top <= -1) {
+			throw new StackEmptyException(top);
+		}
 		System.out.println("pop, stack[" + top + "] = " + stack[top]);
 		return stack[top--];
 	}	
 	public static void main(String[] chickens) {
 		Stack stack1 = new Stack();
 		try{
-			stack1.push(1);
-			stack1.push(2);
-			stack1.push(3);
-			stack1.push(4);
-			stack1.push(5);
+			for (int i=0; i<3; i++){
+				stack1.push(4);
+			}
 		} catch(StackFullException e1){
-			System.out.println("stack is exploding!");
+			System.out.println("===================stack is exploding!====================");
+			System.out.println("top is " + e1.getTop());
 			e1.printStackTrace();
 		}
-		System.out.println("===========" + stack1.top);
+		System.out.println("~~~~~~~~~top  =  " + stack1.top);
 		try{
-			stack1.pop();
-			stack1.pop();
-			stack1.pop();
-			stack1.pop();
-			stack1.pop();
-			stack1.pop();
-			stack1.pop();
-			stack1.pop();
-		} catch(ArrayIndexOutOfBoundsException eee){
-			System.out.println("exception thrown: " + eee);
-			//throw eee;
+			for (int i=0; i<35; i++){
+				stack1.pop();
+			}
+		} catch(StackEmptyException e2){
+			System.out.println("s===================stack is imploding!====================");
+			System.out.println("top is " + e2.getTop());
+			e2.printStackTrace();
 		}
 		
-		System.out.println("===========" + stack1.top);
+		System.out.println("%%%%%%%%%%%%%%top  =  " + stack1.top);
 		
 	}
 }
